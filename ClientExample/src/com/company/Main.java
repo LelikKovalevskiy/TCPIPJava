e07075b96a2d87ff56b22fbb647645a4718a76f3
Алексей <lelik.kovalevskiy@gmail.com>
2014-05-03 20:16:45 +0400

    ...

--- ClientExample/src/com/company/Main.java
+++ ClientExample/src/com/company/Main.java
@@ -1,12 +1,11 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.Scanner;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.PrintStream;
+        import java.net.Socket;
+        import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -40,8 +39,6 @@ public class Main {
                     if (msg.equals("r") || msg.equals("l"))
                         break;
                 }
-                msg = scanner.nextLine();
-                ps.println(Byte.);
                 ps.flush();
                 System.out.println("\nЖдите хода соперника...");
 
